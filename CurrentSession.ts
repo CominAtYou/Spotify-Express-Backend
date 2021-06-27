a0d6e4334fcb6bab369831c0beb11c579f021357
William M <cominatyou@cominatyou.com>
2021-06-26 19:43:49 -0500

    First commit

---
+++ CurrentSession.ts
@@ -0,0 +1,61 @@
+﻿export interface CurrentSession {
+  context: Context;
+  timestamp: number;
+  progress_ms: number;
+  is_playing: boolean;
+  currently_playing_type: string;
+  item: Item;
+}
+export interface Context {
+  external_urls: ExternalUrls;
+  href: string;
+  type: string;
+  uri: string;
+}
+export interface ExternalUrls {
+  spotify: string;
+}
+export interface Item {
+  album: Album;
+  artists?: (ArtistsEntity)[] | null;
+  available_markets?: (string)[] | null;
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: ExternalIds;
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  name: string;
+  popularity: number;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
+}
+export interface Album {
+  album_type: string;
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  images?: (ImagesEntity)[] | null;
+  name: string;
+  type: string;
+  uri: string;
+}
+export interface ImagesEntity {
+  height: number;
+  url: string;
+  width: number;
+}
+export interface ArtistsEntity {
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  name: string;
+  type: string;
+  uri: string;
+}
+export interface ExternalIds {
+  isrc: string;
+}
---
+++ index.ts
@@ -0,0 +1,50 @@
+import { CurrentSession } from './CurrentSession';
+import formUrlEncoded = require('form-urlencoded');
+import express = require('express');
+import fs = require('fs');
+import h = require('axios');
+const axios = h.default;
+const app = express();
+
+let config: {refresh_token: string, client_id: string, client_secret: string} = JSON.parse(fs.readFileSync('./config.json').toString());
+
+const refresh_token: string = config.refresh_token;
+let access_token: string;
+
+interface SpotifyResponse {
+    access_token: string,
+    token_type: string,
+    expires_in: number,
+    refresh_token: string,
+    scope: string,
+    error?: string,
+    error_description?: string;
+}
+
+async function refreshToken() {
+    const d: {data: SpotifyResponse} = await axios.post("https://accounts.spotify.com/api/token", formUrlEncoded({grant_type: "refresh_token", refresh_token: refresh_token}), {headers: {Authorization: `Basic ${Buffer.from(`${config.client_id}:${config.client_secret}`).toString('base64')}`}})
+    access_token = d.data.access_token;
+}
+
+async function getSpotifySong() {
+    const d = await axios.get("https://api.spotify.com/v1/me/player/currently-playing?market=US", {headers: {Authorization: `Bearer ${access_token}`}})
+    const songData: CurrentSession = d.data;
+    const resData = {
+        track: songData.item.name,
+        artist: songData.item.artists[0].name
+    }
+    return resData;
+}
+
+setInterval(async () => {
+    refreshToken();
+}, 3600000);
+refreshToken();
+
+app.get('/spotify', async (req, res) => {
+    res.status(200);
+    res.send(JSON.stringify(await getSpotifySong()));
+});
+
+app.listen(8001);
+console.log("Ready");
