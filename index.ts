95f4c6ae0f9172c781a78ef9a848e696ddb08212
William M <cominatyou@cominatyou.com>
2021-07-04 16:56:01 -0500

    Update once every five seconds

--- index.ts
+++ index.ts
@@ -6,10 +6,17 @@ import h = require('axios');
 const axios = h.default;
 const app = express();
 
+
 let config: {refresh_token: string, client_id: string, client_secret: string} = JSON.parse(fs.readFileSync('./config.json').toString());
 
 const refresh_token: string = config.refresh_token;
 let access_token: string;
+
+interface CurrentTrackDetails {
+    track: string;
+    artist: string;
+    is_playing: boolean;
+}
 
 interface SpotifyResponse {
     access_token: string,
@@ -21,17 +28,31 @@ interface SpotifyResponse {
     error_description?: string;
 }
 
+let currentTrackDetails: CurrentTrackDetails = {
+    track: null,
+    artist: null,
+    is_playing: false,
+}
+
 async function refreshToken() {
     const d: {data: SpotifyResponse} = await axios.post("https://accounts.spotify.com/api/token", formUrlEncoded({grant_type: "refresh_token", refresh_token: refresh_token}), {headers: {Authorization: `Basic ${Buffer.from(`${config.client_id}:${config.client_secret}`).toString('base64')}`}})
     access_token = d.data.access_token;
 }
 
-async function getSpotifySong() {
+async function updateSpotifySong() {
     const d = await axios.get("https://api.spotify.com/v1/me/player/currently-playing?market=US", {headers: {Authorization: `Bearer ${access_token}`}})
     const songData: CurrentSession = d.data;
     // Spotify returns 204 when no songs are playing.
-    const resData = d.status === 204 ? { track: null, artist: null, is_playing: false } : { track: songData.item.name, artist: songData.item.artists[0].name, is_playing: songData.is_playing };
-    return resData;
+    if (d.status === 204) {
+        currentTrackDetails.track = null;
+        currentTrackDetails.artist = null;
+        currentTrackDetails.is_playing = false;
+    }
+    else {
+        currentTrackDetails.track = songData.item.name;
+        currentTrackDetails.artist = songData.item.artists[0].name;
+        currentTrackDetails.is_playing = songData.is_playing;
+    }
 }
 
 setInterval(() => {
@@ -39,9 +60,14 @@ setInterval(() => {
 }, 3600000);
 refreshToken();
 
+setInterval(() => {
+    updateSpotifySong();
+}, 5000);
+updateSpotifySong();
+
 app.get('/spotify', async (_req, res) => {
     res.status(200);
-    res.send(JSON.stringify(await getSpotifySong()));
+    res.send(JSON.stringify(currentTrackDetails));
 });
 
 app.listen(8001);
