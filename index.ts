2895ff4b6d61493fa6c2e74309999e0311e75884
William M <cominatyou@cominatyou.com>
2021-07-07 16:43:09 -0500

    Use require instead of readFileSync for JSON

--- index.ts
+++ index.ts
@@ -1,15 +1,13 @@
 import { CurrentSession } from './CurrentSession';
 import formUrlEncoded = require('form-urlencoded');
 import express = require('express');
-import fs = require('fs');
 import h = require('axios');
 const axios = h.default;
 const app = express();
 
+const config: { refresh_token: string, client_id: string, client_secret: string; } = require('./config.json');
 
-let config: {refresh_token: string, client_id: string, client_secret: string} = JSON.parse(fs.readFileSync('./config.json').toString());
-
-const refresh_token: string = config.refresh_token;
+const refresh_token = config.refresh_token;
 let access_token: string;
 
 interface CurrentTrackDetails {
@@ -32,15 +30,15 @@ let currentTrackDetails: CurrentTrackDetails = {
     track: null,
     artist: null,
     is_playing: false,
-}
+};
 
 async function refreshToken() {
-    const d: {data: SpotifyResponse} = await axios.post("https://accounts.spotify.com/api/token", formUrlEncoded({grant_type: "refresh_token", refresh_token: refresh_token}), {headers: {Authorization: `Basic ${Buffer.from(`${config.client_id}:${config.client_secret}`).toString('base64')}`}})
+    const d: { data: SpotifyResponse; } = await axios.post("https://accounts.spotify.com/api/token", formUrlEncoded({ grant_type: "refresh_token", refresh_token: refresh_token }), { headers: { Authorization: `Basic ${Buffer.from(`${config.client_id}:${config.client_secret}`).toString('base64')}` } });
     access_token = d.data.access_token;
 }
 
 async function updateSpotifySong() {
-    const d = await axios.get("https://api.spotify.com/v1/me/player/currently-playing?market=US", {headers: {Authorization: `Bearer ${access_token}`}})
+    const d = await axios.get("https://api.spotify.com/v1/me/player/currently-playing?market=US", { headers: { Authorization: `Bearer ${access_token}` } });
     const songData: CurrentSession = d.data;
     // Spotify returns 204 when no songs are playing.
     if (d.status === 204) {
